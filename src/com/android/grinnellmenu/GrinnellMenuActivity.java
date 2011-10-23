93f01df77175402e62eae764bbd3ff76e4cbf4c1
dcow <dcow90@gmail.com>
2011-10-23 16:00:28 -0500

    Changed the venues list to an ExpandableListView and populated the group entries with the default venues listing.

--- src/com/android/grinnellmenu/GrinnellMenuActivity.java
+++ src/com/android/grinnellmenu/GrinnellMenuActivity.java
@@ -1,41 +1,106 @@
 package com.android.grinnellmenu;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import android.app.ListActivity;
+import android.app.ExpandableListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.ExpandableListView.OnChildClickListener;
 
-public class GrinnellMenuActivity extends ListActivity {
+public class GrinnellMenuActivity extends ExpandableListActivity {
 
 	public static int GET_DATE = 1;
-	GregorianCalendar mRequestedDate;
+	protected GregorianCalendar mRequestedDate;
+	
+	protected List<Map<String, String>> mGroupList;
+	protected List<List<Map<String, String>>> mChildList;
+	public static String VENUE = "venue";
+	public static String ENTREE = "entree";
+	
+	private ExpandableListAdapter mSELAdapter;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+ 
+		
+		/* Initialize the ExpandableListView. */
+		//Setup the GroupList:
+		mGroupList = new ArrayList<Map<String, String>>();
+		String[] venues = getResources().getStringArray(R.array.venues);
+		for (int i = 0; i < venues.length; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put(VENUE, venues[i]);
+			mGroupList.add(map);			
+		}
+		
+		//Setup the ChildLists to be empty at first since we have not read data yet:
+		mChildList = new ArrayList<List<Map<String, String>>>(); 
+		//--temporary default children values --
+		String empty = getResources().getString(R.string.empty);
+		for (int i = 0; i < venues.length; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put(ENTREE, empty);
+			List<Map<String, String>> entrees = new ArrayList<Map<String, String>>();
+			entrees.add(map);
+			mChildList.add(entrees);
+		}
+		
+		//-- --
+		//Setup the entrees:
+		populateEntrees();
+		
+		//Setup the adapter:
+		mSELAdapter = new SimpleExpandableListAdapter(
+				this,
+				mGroupList,
+				R.layout.venue_field,
+				new String[] { VENUE },
+				new int[] { R.id.venueBox },
+				mChildList,
+				R.layout.entree_field,
+				new String[] { ENTREE },
+				new int[] { R.id.entreeBox }
+				);
+		
+		setListAdapter(mSELAdapter);
+		
+		/* Register Entree Click Listeners */
+		ExpandableListView elv = getExpandableListView();
+		elv.setOnChildClickListener(new OnEntreeClick());
 
-		ListView lv = getListView();
-		lv.setOnItemClickListener(new OnItemClick());
-
+		/* Get the current date and store as the default requested date. */
 		mRequestedDate = new GregorianCalendar();
 		updateMenu();
 
 	}
 
-	private class OnItemClick implements OnItemClickListener {
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
+	private void populateEntrees() {
+		
+		
+	}
 
-			startActivityForResult(new Intent(view.getContext(),
-					MenuItemDetails.class), position);
+	private class OnEntreeClick implements OnChildClickListener {
+		@Override
+		public boolean onChildClick(ExpandableListView parent, View v,
+				int groupPosition, int childPosition, long id) {
+			
+			Long p = ExpandableListView.getPackedPositionForChild(groupPosition, childPosition);
+			startActivityForResult(new Intent(v.getContext(),
+					MenuItemDetails.class), parent.getFlatListPosition(p));
+			
+			return false;
 		}
 	}
 
