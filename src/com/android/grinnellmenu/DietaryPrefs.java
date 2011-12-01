cf5b876493ec4444f1ba7f0e974865e3046912a2
dcow <dcow90@gmail.com>
2011-11-30 20:46:54 -0600

    added the source file too

---
+++ src/com/android/grinnellmenu/DietaryPrefs.java
@@ -0,0 +1,25 @@
+package com.android.grinnellmenu;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class DietaryPrefs extends PreferenceActivity {
+
+	/* Bundle Keys */
+	public static final String DP_KEY = "dietary_prefs";
+	
+	/* Dish Preferences */
+	private int mDishPrefs;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    
+	    /* Obtain the current preferences from the user */
+	    mDishPrefs = getIntent().getExtras().getInt(DP_KEY);
+	
+	    addPreferencesFromResource(R.xml.dietary_prefs);
+	}
+
+}
