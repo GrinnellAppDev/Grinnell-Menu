4cac0fa2c2954b627dfb5e8aa87063feea9b5a0b
linhpha <phamlinh@grinnell.edu>
2018-02-25 14:17:42 -0600

    Add progress bar when loaing web view

--- glicious/src/main/java/edu/grinnell/glicious/BrowserActivity.java
+++ glicious/src/main/java/edu/grinnell/glicious/BrowserActivity.java
@@ -2,6 +2,7 @@ package edu.grinnell.glicious;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,10 +12,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class BrowserActivity extends Activity {
 
     WebView mWebView;
+    ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,31 @@ public class BrowserActivity extends Activity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setInitialScale(100);
         String url = getApplicationContext().getResources().getString(R.string.app_url);
+        mProgressDialog = new ProgressDialog(BrowserActivity.this);
+        mProgressDialog.setMessage("Loading...");
+        mProgressDialog.show();
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(BrowserActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         mWebView.loadUrl(url);
     }
 
